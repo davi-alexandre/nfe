d2b4d7334f25338fca8e72c7c00174fdb202ebd3
Davi Alexandre <deltreertl@gmail.com>
2024-04-21 01:07:50 -0300

    init

---
+++ src/com/exemplo/Main.java
@@ -0,0 +1,8 @@
+package com.exemplo;
+
+public class Main {
+  public static void main(String[] args) {
+    NFeConfigTeste config = new NFeConfigTeste();
+    System.out.println(config.getCertificadoSenha());
+  }
+}
